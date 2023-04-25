3c95a95236fd25b9d9bbfaab2c2a10d622ecb39e
rowanabdelrahim <rawanmuhammad512@gmail.com>
2023-04-24 19:51:07 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A program that prints the alphabet in reverse.
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
