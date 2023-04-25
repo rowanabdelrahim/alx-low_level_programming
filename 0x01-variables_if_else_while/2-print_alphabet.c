177a9ec541fdd9ab7bea221a742f838886bd8386
rowanabdelrahim <rawanmuhammad512@gmail.com>
2023-04-24 18:34:16 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,14 @@
 
 int main(void)
 {
-	char ch = "a";
+	char ch = 'a';
 
-	while (ch <= "z")
-{
-	putchar(ch);
-	ch++;
-}
-putchar("\n");
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+
+	return (0);
 }
