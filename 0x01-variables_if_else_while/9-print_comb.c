a3f4a355cf6fe1ac290064cd73c7439e774138ea
rowanabdelrahim <rawanmuhammad512@gmail.com>
2023-04-28 10:33:00 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,13 @@ int main(void)
 {
 	int digit = 0;
 
-	while (digit < 10)
+	while (digit <= 9)
 	{
-		putchar(digit + 0);
-		if (digit < 9)
+		putchar(digit + 48);
+		if (digit != 9)
 		{
 			putchar(',');
 			putchar(' ');
-		}
-		else
-		{
-			putchar('$');
 		}
 		digit++;
 	}
