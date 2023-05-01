0a374614f11d806493454cd6da1fe7430cdd5b7a
rowanabdelrahim <rawanmuhammad512@gmail.com>
2023-05-01 14:20:22 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * Description: Alphabet
+ * Return: Always (0)
+*/
+
+int main(void)
+{
+	int a = 0 + 97;
+
+	while (a <= 120)
+	{
+		_putchar (a);
+		a++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
