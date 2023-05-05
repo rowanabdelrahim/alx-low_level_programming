483866ffca909c81b1fa4def218bfb8630365445
rowanabdelrahim <rawanmuhammad512@gmail.com>
2023-05-04 17:17:48 -0700

    z

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * string_troupper - change all lowercase to uppercase
+ * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
  * Return: n
 */
 
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
