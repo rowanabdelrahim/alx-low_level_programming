cec8d1af1789364581c04251a3046a871b9397aa
rowanabdelrahim <rawanmuhammad512@gmail.com>
2023-05-08 14:53:13 -0700

    str

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchr - fills memory with a constant byte.
+ * *_strchr - fills memory with a constant byte.
  * @s: pointer to put the constant
  * @c: constant
  * Return: pointer to s
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] >= '\0' ; i++)
 	{
 		if (s[i] == c)
 		{
